1aeb0615d805f2b707814cf5d0cb3ddd8df773f3
matsjj <matsjj@gmail.com>
2015-09-21 15:45:23 +0100

    Basic objects for P2P gossip network..

---
+++ thunder-core/src/main/java/network/thunder/core/communication/objects/p2p/MetaDataBroadcast.java
@@ -0,0 +1,52 @@
+package network.thunder.core.communication.objects.p2p;
+
+import network.thunder.core.communication.objects.subobjects.ConnectedPeer;
+
+import java.util.ArrayList;
+
+/**
+ * object for housing the information that will be broadcasted through the network.
+ */
+public class MetaDataBroadcast {
+
+	private byte[] pubkey;
+	private int timestamp;
+	private byte[] signature;
+	private String ip;
+	private int port;
+
+	private ArrayList<ConnectedPeer> peerList;
+
+	public MetaDataBroadcast (byte[] pubkey, int timestamp, byte[] signature, String ip, int port, ArrayList<ConnectedPeer> peerList) {
+		this.pubkey = pubkey;
+		this.timestamp = timestamp;
+		this.signature = signature;
+		this.ip = ip;
+		this.port = port;
+		this.peerList = peerList;
+	}
+
+	public byte[] getPubkey () {
+		return pubkey;
+	}
+
+	public int getTimestamp () {
+		return timestamp;
+	}
+
+	public byte[] getSignature () {
+		return signature;
+	}
+
+	public String getIp () {
+		return ip;
+	}
+
+	public int getPort () {
+		return port;
+	}
+
+	public ArrayList<ConnectedPeer> getPeerList () {
+		return peerList;
+	}
+}
