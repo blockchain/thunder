597a218ff99f815c5715ac402bb5314012229153
matsjj <matsjj@gmail.com>
2015-10-21 10:02:27 +0100

    Extended P2PContext to hold global IP list.

--- thunder-core/src/main/java/network/thunder/core/communication/nio/P2PContext.java
+++ thunder-core/src/main/java/network/thunder/core/communication/nio/P2PContext.java
@@ -1,10 +1,14 @@
 package network.thunder.core.communication.nio;
 
 import network.thunder.core.communication.Node;
+import network.thunder.core.communication.objects.p2p.PubkeyChannelObject;
+import network.thunder.core.communication.objects.p2p.PubkeyIPObject;
 import network.thunder.core.database.DatabaseHandler;
 
 import javax.sql.DataSource;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 /**
  * Created by matsjerratsch on 14/10/2015.
@@ -12,28 +16,83 @@ import java.util.ArrayList;
 public class P2PContext {
 
 	public static final String HOST = "127.0.0.1";
-	public static final int PORT = 8892;
+	public int port = 8892;
 
-	private ArrayList<Node> connectedNodes = new ArrayList<>();
+	public ArrayList<Node> connectedNodes = new ArrayList<>();
+	public ArrayList<PubkeyIPObject> IPList;
+	public ArrayList<Node> activeNodes;
 
-	P2PServer server = new P2PServer();
-	P2PClient client = new P2PClient();
+	public HashMap<String, PubkeyChannelObject> pubkeyChannelObjectHashMap = new HashMap<>();
 
-	public P2PContext(DataSource dataSource) throws Exception {
+	P2PServer server;
+	P2PClient client;
 
+	public P2PContext (int port) {
+		IPList = new ArrayList<>();
+		activeNodes = new ArrayList<>();
+		this.port = port;
+	}
+
+	public P2PContext (DataSource dataSource) throws Exception {
+		IPList = DatabaseHandler.getIPAddresses(dataSource.getConnection());
+		activeNodes = DatabaseHandler.getNodesWithOpenChanels(dataSource.getConnection());
+	}
+
+	public void startConnections () throws Exception {
 		//Open up for incoming connections
-		server.startServer(8892, connectedNodes);
 
 
-		ArrayList<Node> activeNodes = DatabaseHandler.getNodesWithOpenChanels(dataSource.getConnection());
-
-		for(Node node : activeNodes) {
-			new P2PClient().connectTo(node);
+		for (Node node : activeNodes) {
+			new P2PClient(this).connectTo(node);
 			connectedNodes.add(node);
 		}
 
+		server = new P2PServer(this);
+		server.startServer(port, connectedNodes);
+	}
+
+	public void newIPList (ArrayList<PubkeyIPObject> newList) {
+		boolean newPubKey = true;
+		for (PubkeyIPObject newIP : newList) {
+			for (PubkeyIPObject oldIP : IPList) {
+				if (Arrays.equals(newIP.pubKey, oldIP.pubKey)) {
+					newPubKey = false;
+					if (newIP.timestamp > oldIP.timestamp) {
+						//Got a new IP address for an existing pubkey
+					} else {
+						//Our IP address is newer than his one?
+						//TODO: Send the more recent IP address back?
+					}
+				}
+			}
+			if (newPubKey) {
+				//TODO: Got a pubkey we don't know about yet. Should probably do some checking here
+				IPList.add(newIP);
+			}
+		}
+	}
+
+	public void newIPList (PubkeyIPObject newIP) {
+		boolean newPubKey = true;
+		for (PubkeyIPObject oldIP : IPList) {
+			if (Arrays.equals(newIP.pubKey, oldIP.pubKey)) {
+				newPubKey = false;
+				if (newIP.timestamp > oldIP.timestamp) {
+					//Got a new IP address for an existing pubkey
+				} else {
+					//Our IP address is newer than his one?
+					//TODO: Send the more recent IP address back?
+				}
+			}
+		}
+		if (newPubKey) {
+			//TODO: Got a pubkey we don't know about yet. Should probably do some checking here
+			IPList.add(newIP);
+		}
 
 	}
 
-
+	public ArrayList<PubkeyIPObject> getIPList () {
+		return IPList;
+	}
 }
