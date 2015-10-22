06123ca9e57128c67169119564253b1644b4acc1
matsjj <matsjj@gmail.com>
2015-10-22 18:07:59 +0100

    Added sync methods to P2PContext

--- thunder-core/src/main/java/network/thunder/core/communication/nio/P2PContext.java
+++ thunder-core/src/main/java/network/thunder/core/communication/nio/P2PContext.java
@@ -1,14 +1,17 @@
 package network.thunder.core.communication.nio;
 
-import network.thunder.core.communication.Node;
+import network.thunder.core.mesh.Node;
 import network.thunder.core.communication.objects.p2p.PubkeyChannelObject;
 import network.thunder.core.communication.objects.p2p.PubkeyIPObject;
+import network.thunder.core.communication.objects.p2p.TreeMapDatastructure;
 import network.thunder.core.database.DatabaseHandler;
 
 import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Random;
 
 /**
  * Created by matsjerratsch on 14/10/2015.
@@ -25,12 +28,28 @@ public class P2PContext {
     public HashMap<String, PubkeyChannelObject> pubkeyChannelObjectHashMap = new HashMap<>();
 
     P2PServer server;
-    P2PClient client;
+
+    boolean keepReconnectingToNewNodes = false;
+
+    public boolean fetchFreshIPs = true;
+
+    public TreeMapDatastructure syncDatastructure = new TreeMapDatastructure();
+
+    P2PContext context;
+
+    public DataSource dataSource;
 
     public P2PContext (int port) {
         IPList = new ArrayList<>();
         activeNodes = new ArrayList<>();
         this.port = port;
+        context = this;
+
+        try {
+            dataSource = DatabaseHandler.getDataSource();
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
     }
 
     public P2PContext (DataSource dataSource) throws Exception {
@@ -43,11 +62,77 @@ public class P2PContext {
 
         for (Node node : activeNodes) {
             new P2PClient(this).connectTo(node);
-            connectedNodes.add(node);
         }
 
         server = new P2PServer(this);
         server.startServer(port, connectedNodes);
+    }
+
+    public void startInitialSyncing () {
+        //TODO: Have some initial bootstrap node hardcoded here..
+        Node initialNode = new Node("127.0.0.1", 8992);
+        initialNode.justFetchNewIpAddresses = true;
+
+        try {
+            new P2PClient(this).connectTo(initialNode);
+
+            initialNode.setOnConnectionCloseListener(new Node.OnConnectionCloseListener() {
+                @Override
+                public void onClose () {
+                    System.out.println("Initial Node IP getting done..");
+                    startConnectingToRandomNodes();
+                }
+            });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void startConnectingToRandomNodes () {
+        keepReconnectingToNewNodes = true;
+        new Thread(new Runnable() {
+            @Override
+            public void run () {
+                while (!syncDatastructure.fullySynchronized()) {
+
+                    try {
+                        if (connectedNodes.size() < 10) {
+                            if(IPList.size() > 0) {
+                                PubkeyIPObject ipObject = IPList.get(new Random().nextInt(IPList.size()));
+                                Node node = new Node(ipObject.IP, ipObject.port);
+                                node.justDownloadSyncData = true;
+
+                                new P2PClient(context).connectTo(node);
+                                IPList.remove(ipObject); //TODO: Maybe not the best to just delete it here?
+                            }
+                        }
+
+                        Thread.sleep(5000);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+                stopConnectingToRandomNodes();
+            }
+        }).start();
+    }
+
+    public void stopConnectingToRandomNodes () {
+        keepReconnectingToNewNodes = false;
+        //Drop all connections..
+        for (Node node : connectedNodes) {
+            node.getNettyContext().close();
+        }
+
+        System.out.println("Received all sync data...");
+
+        //TODO: Validate all anchors we received. We need some kind of full blockchain to do that..
+
+        //Now figure out to which nodes we want to build lightning channels..
     }
 
     public void newIPList (ArrayList<PubkeyIPObject> newList) {
@@ -84,10 +169,10 @@ public class P2PContext {
                 }
             }
         }
-        if (newPubKey) {
+//        if (newPubKey) {
             //TODO: Got a pubkey we don't know about yet. Should probably do some checking here
             IPList.add(newIP);
-        }
+//        }
 
     }
 
