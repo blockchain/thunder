d73a2619555449cf96206d7266e783c26a6b90ec
matsjj <matsjj@gmail.com>
2015-10-26 19:26:44 +0000

    Added database mapping for syncing

--- thunder-core/src/main/java/network/thunder/core/database/DatabaseHandler.java
+++ thunder-core/src/main/java/network/thunder/core/database/DatabaseHandler.java
@@ -1,20 +1,20 @@
 package network.thunder.core.database;
 
+import com.mchange.v2.c3p0.C3P0ProxyStatement;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
-import network.thunder.core.communication.objects.p2p.ChannelStatusObject;
 import network.thunder.core.communication.objects.p2p.DataObject;
-import network.thunder.core.communication.objects.p2p.PubkeyChannelObject;
-import network.thunder.core.communication.objects.p2p.PubkeyIPObject;
+import network.thunder.core.communication.objects.p2p.P2PDataObject;
+import network.thunder.core.communication.objects.p2p.TreeMapDatastructure;
+import network.thunder.core.communication.objects.p2p.sync.ChannelStatusObject;
+import network.thunder.core.communication.objects.p2p.sync.PubkeyChannelObject;
+import network.thunder.core.communication.objects.p2p.sync.PubkeyIPObject;
 import network.thunder.core.database.objects.Channel;
 import network.thunder.core.etc.Tools;
 import network.thunder.core.mesh.Node;
 
 import javax.sql.DataSource;
 import java.beans.PropertyVetoException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -69,6 +69,340 @@ public class DatabaseHandler {
             }
             result.close();
             return channelList;
+        } finally {
+            if (stmt != null) {
+                stmt.close();
+            }
+        }
+    }
+
+    public static int getChannelId (Connection conn, byte[] pubkeyA, byte[] pubkeyB) throws SQLException {
+
+        PreparedStatement stmt = null;
+        try {
+
+//            int nodeAID = DatabaseHandler.getNodeId(conn, pubkeyA);
+//            int nodeBID = DatabaseHandler.getNodeId(conn, pubkeyB);
+
+//            //Lets see if we have the object in the db already
+            stmt = conn.prepareStatement("SELECT channels.id FROM channels " +
+                                             "INNER JOIN nodes AS nodes_a_table ON nodes_a_table.id = channels.node_id_a " +
+                                             "INNER JOIN nodes AS nodes_b_table ON nodes_b_table.id = channels.node_id_b " +
+                                             "WHERE ((nodes_a_table.pubkey = ? AND nodes_b_table.pubkey=?) OR (nodes_a_table.pubkey = ? AND nodes_b_table.pubkey=?))");
+
+            stmt.setBytes(1, pubkeyA);
+            stmt.setBytes(2, pubkeyB);
+            stmt.setBytes(3, pubkeyB);
+            stmt.setBytes(4, pubkeyA);
+
+//            //Lets see if we have the object in the db already
+//            stmt = conn.prepareStatement("SELECT channels.id FROM channels " + "WHERE ((node_id_a = ? AND node_id_b=?) OR (node_id_a = ? AND node_id_b=?))");
+//
+//            stmt.setInt(1, nodeAID);
+//            stmt.setInt(2, nodeBID);
+//            stmt.setInt(3, nodeBID);
+//            stmt.setInt(4, nodeAID);
+
+//            try {
+//                printInnerStatement((C3P0ProxyStatement) stmt);
+//            } catch (IllegalAccessException e) {
+//                e.printStackTrace();
+//            } catch (InvocationTargetException e) {
+//                e.printStackTrace();
+//            } catch (NoSuchMethodException e) {
+//                e.printStackTrace();
+//            }
+
+            ResultSet set = stmt.executeQuery();
+
+            if (!set.first()) {
+                return 0;
+            }
+
+            return set.getInt("channels.id");
+
+        } finally {
+            if (stmt != null) {
+                stmt.close();
+            }
+        }
+    }
+
+    static void printInnerStatement (C3P0ProxyStatement stmt) {
+        try {
+            java.lang.reflect.Method m = java.io.PrintStream.class.getMethod("println", new Class[]{Object.class});
+            stmt.rawStatementOperation(m, System.out, new Object[]{C3P0ProxyStatement.RAW_STATEMENT});
+        } catch (Exception e) {
+        }
+    }
+
+    public static int getNodeId (Connection conn, byte[] pubkey) throws SQLException {
+
+        PreparedStatement stmt = null;
+        try {
+
+            //Lets see if we have the object in the db already
+            stmt = conn.prepareStatement("SELECT nodes.id FROM nodes WHERE nodes.pubkey=?");
+            System.out.println(stmt);
+
+            stmt.setBytes(1, pubkey);
+
+            ResultSet set = stmt.executeQuery();
+
+            if (!set.first()) {
+                //New node. Might as well just insert it now?
+                stmt.close();
+                set.close();
+
+                stmt = conn.prepareStatement("INSERT INTO nodes VALUES(?,?)", PreparedStatement.RETURN_GENERATED_KEYS);
+
+                stmt.setInt(1, 0);
+                stmt.setBytes(2, pubkey);
+
+                set = stmt.getGeneratedKeys();
+                int id = set.getInt(0);
+
+                set.close();
+                return id;
+            }
+
+            int id = set.getInt("nodes.id");
+            return id;
+
+        } finally {
+            if (stmt != null) {
+                stmt.close();
+            }
+        }
+    }
+
+    public static int getIPId (Connection conn, byte[] pubkey) throws SQLException {
+
+        PreparedStatement stmt = null;
+        try {
+
+            //Lets see if we have the object in the db already
+            stmt = conn.prepareStatement("SELECT pubkey_ips.id FROM pubkey_ips" +
+                                             " INNER JOIN nodes ON nodes.id=pubkey_ips.node_id" +
+                                             " WHERE nodes.pubkey=?");
+
+            stmt.setBytes(1, pubkey);
+
+            ResultSet set = stmt.executeQuery();
+
+            if (!set.first()) {
+                return 0;
+            }
+            int id = set.getInt("pubkey_ips.id");
+            set.close();
+            return id;
+
+        } finally {
+            if (stmt != null) {
+                stmt.close();
+            }
+        }
+    }
+
+    public static int newChannel (Connection conn, PubkeyChannelObject pubkeyChannelObject) throws SQLException {
+
+        PreparedStatement stmt = null;
+        try {
+
+            //Check if we have the channel already..
+            int id = getChannelId(conn, pubkeyChannelObject.pubkeyA, pubkeyChannelObject.pubkeyB);
+
+            if (id != 0) {
+                //TODO: We already have this channel in our database. Maybe change the object? These should be fairly static though..
+                stmt = conn.prepareStatement("UPDATE channels SET fragment_index=?, secret_a_hash=?, secret_b_hash=?, pubkey_a1=?, pubkey_a2=?, pubkey_b1=?, " +
+                                                 "pubkey_b2=?, " +
+                                                 "txid_anchor=?, signature_a=?, signature_b=? WHERE id=?");
+
+                stmt.setInt(1, TreeMapDatastructure.objectToFragmentIndex(pubkeyChannelObject));
+                stmt.setBytes(2, pubkeyChannelObject.secretAHash);
+                stmt.setBytes(3, pubkeyChannelObject.secretBHash);
+                stmt.setBytes(4, pubkeyChannelObject.pubkeyA1);
+                stmt.setBytes(5, pubkeyChannelObject.pubkeyA2);
+                stmt.setBytes(6, pubkeyChannelObject.pubkeyB1);
+                stmt.setBytes(7, pubkeyChannelObject.pubkeyB2);
+                stmt.setBytes(8, pubkeyChannelObject.txidAnchor);
+                stmt.setBytes(9, pubkeyChannelObject.signatureA);
+                stmt.setBytes(10, pubkeyChannelObject.signatureB);
+                stmt.setInt(11, id);
+
+                printInnerStatement((C3P0ProxyStatement) stmt);
+                stmt.execute();
+
+                //TODO: Actually return the newly generated id..
+                return id;
+            }
+
+            //We don't have the channel currently.. Let's insert it
+            int nodeIdA = DatabaseHandler.getNodeId(conn, pubkeyChannelObject.pubkeyA);
+            int nodeIdB = DatabaseHandler.getNodeId(conn, pubkeyChannelObject.pubkeyB);
+
+            stmt = conn.prepareStatement("INSERT INTO channels VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
+
+            stmt.setInt(1, id);
+            stmt.setInt(2, TreeMapDatastructure.objectToFragmentIndex(pubkeyChannelObject));
+            stmt.setInt(3, nodeIdA);
+            stmt.setInt(4, nodeIdB);
+            stmt.setBytes(5, pubkeyChannelObject.secretAHash);
+            stmt.setBytes(6, pubkeyChannelObject.secretBHash);
+            stmt.setBytes(7, pubkeyChannelObject.pubkeyA1);
+            stmt.setBytes(8, pubkeyChannelObject.pubkeyA2);
+            stmt.setBytes(9, pubkeyChannelObject.pubkeyB1);
+            stmt.setBytes(10, pubkeyChannelObject.pubkeyB2);
+            stmt.setBytes(11, pubkeyChannelObject.txidAnchor);
+            stmt.setBytes(12, pubkeyChannelObject.signatureA);
+            stmt.setBytes(13, pubkeyChannelObject.signatureB);
+            stmt.setBytes(14, pubkeyChannelObject.pubkeyB2);
+            stmt.setBytes(15, pubkeyChannelObject.pubkeyB2);
+
+            printInnerStatement((C3P0ProxyStatement) stmt);
+
+            stmt.execute();
+
+            //TODO: Actually return the newly generated id..
+            return id;
+
+        } finally {
+            if (stmt != null) {
+                stmt.close();
+            }
+        }
+    }
+
+    public static boolean newIPObject (Connection conn, PubkeyIPObject IPObject) throws SQLException {
+
+        PreparedStatement stmt = null;
+        try {
+
+            //Check if we have the IP already..
+            int id = getIPId(conn, IPObject.pubkey);
+
+            if (id != 0) {
+
+                stmt = conn.prepareStatement("UPDATE pubkey_ips SET fragment_index=?, host=?, port=?, timestamp=?, signature=? WHERE id=? AND timestamp<?");
+                stmt.setInt(1, TreeMapDatastructure.objectToFragmentIndex(IPObject));
+                stmt.setString(2, IPObject.IP);
+                stmt.setInt(3, IPObject.port);
+                stmt.setInt(4, IPObject.timestamp);
+                stmt.setBytes(5, IPObject.signature);
+                stmt.setInt(6, id);
+                stmt.setInt(7, IPObject.timestamp);
+
+                int count = stmt.executeUpdate();
+
+                printInnerStatement((C3P0ProxyStatement) stmt);
+
+                return (count > 0);
+            }
+
+            //We don't have the IP currently..
+
+            stmt = conn.prepareStatement("INSERT INTO pubkey_ips VALUES(?,?,?,?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
+
+            stmt.setInt(1, id);
+            stmt.setInt(2, TreeMapDatastructure.objectToFragmentIndex(IPObject));
+            stmt.setInt(3, DatabaseHandler.getNodeId(conn, IPObject.pubkey));
+            stmt.setString(4, IPObject.IP);
+            stmt.setInt(5, IPObject.port);
+            stmt.setInt(6, IPObject.timestamp);
+            stmt.setBytes(7, IPObject.signature);
+
+            printInnerStatement((C3P0ProxyStatement) stmt);
+
+            stmt.execute();
+
+            return true;
+
+        } finally {
+            if (stmt != null) {
+                stmt.close();
+            }
+        }
+    }
+
+    public static int newChannelStatus (Connection conn, ChannelStatusObject channelStatusObject) throws SQLException {
+
+        PreparedStatement stmt = null;
+        try {
+
+            //Lets see if we have the object in the db already
+            stmt = conn.prepareStatement("SELECT channel_status.id, channel_status.channel_id FROM channel_status " +
+                                             "INNER JOIN channels ON channel_status.channel_id = channels.id " +
+                                             "INNER JOIN nodes AS nodes_a_table ON nodes_a_table.id = channels.node_id_a " +
+                                             "INNER JOIN nodes AS nodes_b_table ON nodes_b_table.id = channels.node_id_b " +
+                                             "WHERE ((nodes_a_table.pubkey = ? AND nodes_b_table.pubkey=?) OR (nodes_a_table.pubkey = ? AND nodes_b_table.pubkey=?))");
+
+            stmt.setBytes(1, channelStatusObject.pubkeyA);
+            stmt.setBytes(2, channelStatusObject.pubkeyB);
+            stmt.setBytes(3, channelStatusObject.pubkeyB);
+            stmt.setBytes(4, channelStatusObject.pubkeyA);
+
+            ResultSet set = stmt.executeQuery();
+
+            int id = 0;
+
+            if (!set.first()) {
+                int channelId = getChannelId(conn, channelStatusObject.pubkeyA, channelStatusObject.pubkeyB);
+                if (channelId == 0) {
+                    //TODO: We don't even know the channel yet. Just drop the object..
+                    return 0;
+                }
+
+                //Insert a new channel state object into the database as we don't have it there yet
+                set.close();
+                stmt.close();
+
+                stmt = conn.prepareStatement("INSERT INTO channel_status VALUES(?,?,?,?,?,?,?)", PreparedStatement.RETURN_GENERATED_KEYS);
+
+                stmt.setInt(1, id);
+                stmt.setInt(2, TreeMapDatastructure.objectToFragmentIndex(channelStatusObject));
+                stmt.setInt(3, channelId);
+                stmt.setBytes(4, channelStatusObject.infoA);
+                stmt.setBytes(5, channelStatusObject.infoB);
+                stmt.setInt(6, channelStatusObject.timestamp);
+                stmt.setBytes(7, channelStatusObject.signatureA);
+                stmt.setBytes(8, channelStatusObject.signatureB);
+
+                printInnerStatement((C3P0ProxyStatement) stmt);
+
+                stmt.execute();
+
+                set = stmt.getGeneratedKeys();
+                id = set.getInt(1);
+                set.close();
+                return id;
+
+            } else {
+
+                //Update the channel set that we have right now in the database
+
+                id = set.getInt("channel_status.id");
+                set.close();
+                stmt.close();
+
+                stmt = conn.prepareStatement("UPDATE channel_status SET fragment_index=?, info_a=?, info_b=?, timestamp=?, signature_a=?, signature_b=? " +
+                                                 "WHERE" + " id=?");
+
+                stmt.setInt(1, TreeMapDatastructure.objectToFragmentIndex(channelStatusObject));
+                stmt.setBytes(2, channelStatusObject.infoA);
+                stmt.setBytes(3, channelStatusObject.infoB);
+                stmt.setInt(4, channelStatusObject.timestamp);
+                stmt.setBytes(5, channelStatusObject.signatureA);
+                stmt.setBytes(6, channelStatusObject.signatureB);
+                stmt.setInt(7, id);
+
+                stmt.execute();
+
+                printInnerStatement((C3P0ProxyStatement) stmt);
+
+                return id;
+
+            }
+
         } finally {
             if (stmt != null) {
                 stmt.close();
@@ -286,7 +620,7 @@ public class DatabaseHandler {
 
             stmt = conn.prepareStatement("SELECT * FROM pubkey_ips " +
                                              "INNER JOIN nodes ON nodes.id=pubkey_ips.node_id " +
-                                             "WHERE channel_status.fragment_index=?");
+                                             "WHERE pubkey_ips.fragment_index=?");
 
             stmt.setInt(1, index);
             result = stmt.executeQuery();
@@ -303,6 +637,24 @@ public class DatabaseHandler {
         } finally {
             if (stmt != null) {
                 stmt.close();
+            }
+        }
+    }
+
+    public static void syncDatalist (Connection conn, ArrayList<P2PDataObject> dataList) throws SQLException {
+        for (P2PDataObject object : dataList) {
+            if (object instanceof PubkeyChannelObject) {
+                DatabaseHandler.newChannel(conn, (PubkeyChannelObject) object);
+            }
+        }
+        for (P2PDataObject object : dataList) {
+            if (object instanceof ChannelStatusObject) {
+                DatabaseHandler.newChannelStatus(conn, (ChannelStatusObject) object);
+            }
+        }
+        for (P2PDataObject object : dataList) {
+            if (object instanceof PubkeyIPObject) {
+                DatabaseHandler.newIPObject(conn, (PubkeyIPObject) object);
             }
         }
     }
