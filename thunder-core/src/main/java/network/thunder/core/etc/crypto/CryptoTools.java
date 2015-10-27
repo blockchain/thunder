f98631ecb6ac89cdfcc19beaf6735fe131f173a7
matsjj <matsjj@gmail.com>
2015-10-27 15:43:39 +0000

    Added verifySignature

--- thunder-core/src/main/java/network/thunder/core/etc/crypto/CryptoTools.java
+++ thunder-core/src/main/java/network/thunder/core/etc/crypto/CryptoTools.java
@@ -19,44 +19,9 @@ import java.util.Arrays;
  */
 public class CryptoTools {
 
-	public static byte[] decryptAES_CTR (byte[] data, byte[] keyBytes, byte[] ivBytes, long counter) {
+    public static byte[] decryptAES_CTR (byte[] data, byte[] keyBytes, byte[] ivBytes, long counter) {
 
-		try {
-
-//			byte[] keyBytes = new byte[]{(byte) 0x36, (byte) 0xf1, (byte) 0x83, (byte) 0x57, (byte) 0xbe, (byte) 0x4d, (byte) 0xbd, (byte) 0x77, (byte) 0xf0,
-//					(byte) 0x50, (byte) 0x51, (byte) 0x5c, 0x73, (byte) 0xfc, (byte) 0xf9, (byte) 0xf2};
-
-//			byte[] ivBytes = new byte[]{(byte) 0x69, (byte) 0xdd, (byte) 0xa8, (byte) 0x45, (byte) 0x5c, (byte) 0x7d, (byte) 0xd4, (byte) 0x25, (byte) 0x4b,
-//					(byte) 0xf3, (byte) 0x53, (byte) 0xb7, (byte) 0x73, (byte) 0x30, (byte) 0x4e, (byte) 0xec};
-
-			byte[] ivWithCounter = new byte[16];
-			System.arraycopy(ivBytes, 0, ivWithCounter, 0, ivBytes.length);
-			byte[] counterBytes = ByteBuffer.allocate(Long.SIZE / Byte.SIZE).putLong(counter).array();
-			System.arraycopy(counterBytes, 0, ivWithCounter, ivBytes.length, counterBytes.length);
-
-			//Initialisation
-			SecretKeySpec key = new SecretKeySpec(keyBytes, "AES");
-			IvParameterSpec ivSpec = new IvParameterSpec(ivWithCounter);
-
-			//Mode
-			Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
-
-			cipher.init(Cipher.DECRYPT_MODE, key, ivSpec);
-			byte[] plain = cipher.doFinal(data);
-
-			String plaintext = new String(plain);
-			System.out.println("plaintext: " + plaintext);
-
-			return plain;
-
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	public static byte[] encryptAES_CTR (byte[] data, byte[] keyBytes, byte[] ivBytes, long counter) {
-
-		try {
+        try {
 
 //			byte[] keyBytes = new byte[]{(byte) 0x36, (byte) 0xf1, (byte) 0x83, (byte) 0x57, (byte) 0xbe, (byte) 0x4d, (byte) 0xbd, (byte) 0x77, (byte) 0xf0,
 //					(byte) 0x50, (byte) 0x51, (byte) 0x5c, 0x73, (byte) 0xfc, (byte) 0xf9, (byte) 0xf2};
@@ -64,84 +29,114 @@ public class CryptoTools {
 //			byte[] ivBytes = new byte[]{(byte) 0x69, (byte) 0xdd, (byte) 0xa8, (byte) 0x45, (byte) 0x5c, (byte) 0x7d, (byte) 0xd4, (byte) 0x25, (byte) 0x4b,
 //					(byte) 0xf3, (byte) 0x53, (byte) 0xb7, (byte) 0x73, (byte) 0x30, (byte) 0x4e, (byte) 0xec};
 
-			byte[] ivWithCounter = new byte[16];
-			System.arraycopy(ivBytes, 0, ivWithCounter, 0, ivBytes.length);
-			byte[] counterBytes = ByteBuffer.allocate(Long.SIZE / Byte.SIZE).putLong(counter).array();
-			System.arraycopy(counterBytes, 0, ivWithCounter, ivBytes.length, counterBytes.length);
+            byte[] ivWithCounter = new byte[16];
+            System.arraycopy(ivBytes, 0, ivWithCounter, 0, ivBytes.length);
+            byte[] counterBytes = ByteBuffer.allocate(Long.SIZE / Byte.SIZE).putLong(counter).array();
+            System.arraycopy(counterBytes, 0, ivWithCounter, ivBytes.length, counterBytes.length);
 
-			//Initialisation
-			System.out.println(keyBytes.length);
-			SecretKeySpec key = new SecretKeySpec(keyBytes, "AES");
-			IvParameterSpec ivSpec = new IvParameterSpec(ivWithCounter);
+            //Initialisation
+            SecretKeySpec key = new SecretKeySpec(keyBytes, "AES");
+            IvParameterSpec ivSpec = new IvParameterSpec(ivWithCounter);
 
-			System.out.println(key);
+            //Mode
+            Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
 
-			//Mode
-			Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
+            cipher.init(Cipher.DECRYPT_MODE, key, ivSpec);
+            byte[] plain = cipher.doFinal(data);
 
-			cipher.init(Cipher.DECRYPT_MODE, key, ivSpec);
-			byte[] original = cipher.doFinal(data);
+            String plaintext = new String(plain);
+//			System.out.println("plaintext: " + plaintext);
 
-			String plaintext = new String(original);
-			System.out.println("plaintext: " + plaintext);
+            return plain;
 
-			return original;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
+    public static byte[] encryptAES_CTR (byte[] data, byte[] keyBytes, byte[] ivBytes, long counter) {
 
-	public static byte[] addHMAC (byte[] data, byte[] keyBytes) throws NoSuchAlgorithmException, InvalidKeyException {
-		SecretKeySpec keySpec = new SecretKeySpec(keyBytes, "HmacSHA1");
-		Mac mac = Mac.getInstance("HmacSHA1");
-		mac.init(keySpec);
-		byte[] result = mac.doFinal(data);
+        try {
 
-		byte[] total = new byte[result.length+data.length];
-		System.arraycopy(result, 0, total, 0, result.length);
-		System.arraycopy(data, 0, total, result.length, data.length);
+//			byte[] keyBytes = new byte[]{(byte) 0x36, (byte) 0xf1, (byte) 0x83, (byte) 0x57, (byte) 0xbe, (byte) 0x4d, (byte) 0xbd, (byte) 0x77, (byte) 0xf0,
+//					(byte) 0x50, (byte) 0x51, (byte) 0x5c, 0x73, (byte) 0xfc, (byte) 0xf9, (byte) 0xf2};
 
+//			byte[] ivBytes = new byte[]{(byte) 0x69, (byte) 0xdd, (byte) 0xa8, (byte) 0x45, (byte) 0x5c, (byte) 0x7d, (byte) 0xd4, (byte) 0x25, (byte) 0x4b,
+//					(byte) 0xf3, (byte) 0x53, (byte) 0xb7, (byte) 0x73, (byte) 0x30, (byte) 0x4e, (byte) 0xec};
 
-		return total;
-	}
+            byte[] ivWithCounter = new byte[16];
+            System.arraycopy(ivBytes, 0, ivWithCounter, 0, ivBytes.length);
+            byte[] counterBytes = ByteBuffer.allocate(Long.SIZE / Byte.SIZE).putLong(counter).array();
+            System.arraycopy(counterBytes, 0, ivWithCounter, ivBytes.length, counterBytes.length);
 
-	public static byte[] checkAndRemoveHMAC (byte[] data, byte[] keyBytes) throws NoSuchAlgorithmException, InvalidKeyException {
-		byte[] hmac = new byte[20];
-		byte[] rest = new byte[data.length-hmac.length];
-		System.arraycopy(data, 0, hmac, 0, hmac.length);
-		System.arraycopy(data, hmac.length, rest, 0, data.length-hmac.length);
+            //Initialisation
+//			System.out.println(keyBytes.length);
+            SecretKeySpec key = new SecretKeySpec(keyBytes, "AES");
+            IvParameterSpec ivSpec = new IvParameterSpec(ivWithCounter);
 
+//			System.out.println(key);
 
-		SecretKeySpec keySpec = new SecretKeySpec(keyBytes, "HmacSHA1");
-		Mac mac = Mac.getInstance("HmacSHA1");
-		mac.init(keySpec);
-		byte[] result = mac.doFinal(rest);
+            //Mode
+            Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
 
-		if(Arrays.equals(result, hmac)) {
-			return rest;
-		}
+            cipher.init(Cipher.DECRYPT_MODE, key, ivSpec);
+            byte[] original = cipher.doFinal(data);
 
+            String plaintext = new String(original);
+//			System.out.println("plaintext: " + plaintext);
 
-		throw new RuntimeException("HMAC does not match..");
+            return original;
 
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    public static byte[] addHMAC (byte[] data, byte[] keyBytes) throws NoSuchAlgorithmException, InvalidKeyException {
+        SecretKeySpec keySpec = new SecretKeySpec(keyBytes, "HmacSHA1");
+        Mac mac = Mac.getInstance("HmacSHA1");
+        mac.init(keySpec);
+        byte[] result = mac.doFinal(data);
 
-	}
+        byte[] total = new byte[result.length + data.length];
+        System.arraycopy(result, 0, total, 0, result.length);
+        System.arraycopy(data, 0, total, result.length, data.length);
 
-	public static void verifySignature(ECKey pubkey, byte[] data, byte[] signature) throws NoSuchProviderException, NoSuchAlgorithmException {
-		MessageDigest hashHandler = MessageDigest.getInstance("SHA256", "BC");
-		hashHandler.update(data);
-		byte[] hash = hashHandler.digest();
+        return total;
+    }
 
-		if(!pubkey.verify(hash, signature)) {
-			System.out.println("Signature does not match..");
-			throw new RuntimeException("Signature does not match..");
-		}
-	}
+    public static byte[] checkAndRemoveHMAC (byte[] data, byte[] keyBytes) throws NoSuchAlgorithmException, InvalidKeyException {
+        byte[] hmac = new byte[20];
+        byte[] rest = new byte[data.length - hmac.length];
+        System.arraycopy(data, 0, hmac, 0, hmac.length);
+        System.arraycopy(data, hmac.length, rest, 0, data.length - hmac.length);
 
-	public static byte[] createSignature(ECKey pubkey, byte[] data) throws NoSuchProviderException, NoSuchAlgorithmException {
+        SecretKeySpec keySpec = new SecretKeySpec(keyBytes, "HmacSHA1");
+        Mac mac = Mac.getInstance("HmacSHA1");
+        mac.init(keySpec);
+        byte[] result = mac.doFinal(rest);
 
-		return pubkey.sign(Sha256Hash.of(data)).encodeToDER();
-	}
+        if (Arrays.equals(result, hmac)) {
+            return rest;
+        }
+
+        throw new RuntimeException("HMAC does not match..");
+
+    }
+
+    public static void verifySignature (ECKey pubkey, byte[] data, byte[] signature) throws NoSuchProviderException, NoSuchAlgorithmException {
+        MessageDigest hashHandler = MessageDigest.getInstance("SHA256", "BC");
+        hashHandler.update(data);
+        byte[] hash = hashHandler.digest();
+
+        if (!pubkey.verify(hash, signature)) {
+            System.out.println("Signature does not match..");
+            throw new RuntimeException("Signature does not match..");
+        }
+    }
+
+    public static byte[] createSignature (ECKey pubkey, byte[] data) throws NoSuchProviderException, NoSuchAlgorithmException {
+
+        return pubkey.sign(Sha256Hash.of(data)).encodeToDER();
+    }
 }
