219c761bee8713a8d9fcbc0bfed4fffd959633cf
matsjj <matsjj@gmail.com>
2015-10-29 15:07:06 +0000

    Added mysql schema

---
+++ thunder-core/src/test/java/network/thunder/core/communication/nio/handler/low/EncryptionHandlerTest.java
@@ -0,0 +1,10 @@
+package network.thunder.core.communication.nio.handler.low;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by matsjerratsch on 29/10/2015.
+ */
+public class EncryptionHandlerTest extends TestCase {
+
+}
