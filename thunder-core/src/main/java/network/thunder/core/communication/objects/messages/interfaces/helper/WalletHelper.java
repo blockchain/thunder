3b3b6247cedcd606914d64ac1f89b1a6ae093a1a
matsjj <matsjj@gmail.com>
2016-01-19 14:50:24 +0000

    Added and implemented WalletHelper interface

---
+++ thunder-core/src/main/java/network/thunder/core/communication/objects/messages/interfaces/helper/WalletHelper.java
@@ -0,0 +1,12 @@
+package network.thunder.core.communication.objects.messages.interfaces.helper;
+
+import org.bitcoinj.core.Transaction;
+
+/**
+ * Created by matsjerratsch on 19/01/2016.
+ */
+public interface WalletHelper {
+    long getSpendableAmount();
+
+    Transaction completeInputs(Transaction transaction);
+}
