87afcf6f39163414495b78b2da9b360361b39073
matsjj <matsjj@gmail.com>
2016-01-29 11:45:47 +0000

    Added ConnectionManagerWrapper

---
+++ thunder-core/src/test/java/network/thunder/core/etc/ConnectionManagerWrapper.java
@@ -0,0 +1,14 @@
+package network.thunder.core.etc;
+
+import network.thunder.core.communication.nio.ConnectionManager;
+import network.thunder.core.database.DBHandler;
+import org.bitcoinj.core.Wallet;
+
+/**
+ * Created by matsjerratsch on 26/01/2016.
+ */
+public class ConnectionManagerWrapper {
+    public Wallet wallet;
+    public ConnectionManager connectionManager;
+    public DBHandler dbHandler;
+}
