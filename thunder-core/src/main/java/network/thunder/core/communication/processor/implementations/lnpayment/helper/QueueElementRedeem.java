63d8c616ea370f443332edcf0fe2e2938b4fe043
matsjj <matsjj@gmail.com>
2016-02-04 19:29:16 +0000

    Implemented QueueElementRedeem and Refund

--- thunder-core/src/main/java/network/thunder/core/communication/processor/implementations/lnpayment/helper/QueueElementRedeem.java
+++ thunder-core/src/main/java/network/thunder/core/communication/processor/implementations/lnpayment/helper/QueueElementRedeem.java
@@ -2,17 +2,35 @@ package network.thunder.core.communication.processor.implementations.lnpayment.h
 
 import network.thunder.core.communication.objects.lightning.subobjects.ChannelStatus;
 import network.thunder.core.communication.objects.lightning.subobjects.PaymentData;
+import network.thunder.core.communication.objects.subobjects.PaymentSecret;
 
 /**
  * Created by matsjerratsch on 07/01/2016.
  */
 public class QueueElementRedeem extends QueueElement {
 
-    PaymentData paymentData;
+    PaymentSecret paymentSecret;
+
+    public QueueElementRedeem (PaymentSecret paymentSecret) {
+        this.paymentSecret = paymentSecret;
+    }
 
     @Override
     public ChannelStatus produceNewChannelStatus (ChannelStatus channelStatus) {
         ChannelStatus status = channelStatus.getClone();
+
+        PaymentData paymentData = null;
+        for(PaymentData p : channelStatus.oldPayments) {
+            if(p.secret.equals(paymentSecret))
+                paymentData = p;
+        }
+
+        if(paymentData == null) {
+            //TODO We want to redeem a payment, but apparently it's no longer within the oldpayments?
+            System.out.println("QueueElementRedeem: Can't redeem, not part of old payments..");
+            return channelStatus;
+        }
+
         status.redeemedPayments.add(paymentData);
         status.oldPayments.remove(paymentData);
 
--- thunder-core/src/main/java/network/thunder/core/communication/processor/implementations/lnpayment/helper/QueueElementRefund.java
+++ thunder-core/src/main/java/network/thunder/core/communication/processor/implementations/lnpayment/helper/QueueElementRefund.java
@@ -2,22 +2,42 @@ package network.thunder.core.communication.processor.implementations.lnpayment.h
 
 import network.thunder.core.communication.objects.lightning.subobjects.ChannelStatus;
 import network.thunder.core.communication.objects.lightning.subobjects.PaymentData;
+import network.thunder.core.communication.objects.subobjects.PaymentSecret;
 
 /**
  * Created by matsjerratsch on 07/01/2016.
  */
 public class QueueElementRefund extends QueueElement {
 
-    PaymentData paymentData;
+    PaymentSecret paymentSecret;
+
+    public QueueElementRefund (PaymentSecret paymentSecret) {
+        this.paymentSecret = paymentSecret;
+    }
 
     @Override
     public ChannelStatus produceNewChannelStatus (ChannelStatus channelStatus) {
         ChannelStatus status = channelStatus.getClone();
-        status.refundedPayments.add(paymentData);
-        status.oldPayments.remove(paymentData);
-        //TODO add some disincentives into refunding
+        PaymentData paymentRefund = null;
+        for(PaymentData payment : status.oldPayments) {
+            if(payment.secret.equals(this.paymentSecret)) {
+                paymentRefund = payment;
+            }
+        }
+        if(paymentRefund == null) {
+            //TODO Payment to be refunded is not in old payments..?
+            System.out.println("QueueElementRefund could not find old payment..");
+            return status;
+        } else {
+            status.refundedPayments.add(paymentRefund);
+            status.oldPayments.remove(paymentRefund);
+            //TODO add some disincentives into refunding
 
-        status.amountClient += paymentData.amount;
-        return status;
+            status.amountClient += paymentRefund.amount;
+            return status;
+        }
+
+
+
     }
 }
