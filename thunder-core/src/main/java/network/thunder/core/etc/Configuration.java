e86f5b0a87fd5bed89d47c1003884a64c7768224
matsjj <matsjj@gmail.com>
2016-02-29 13:50:00 +0000

    Added Configuration class to load from JSON

---
+++ thunder-core/src/main/java/network/thunder/core/etc/Configuration.java
@@ -0,0 +1,14 @@
+package network.thunder.core.etc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by matsjerratsch on 11/02/2016.
+ */
+public class Configuration {
+    public String serverKey;
+    public String hostnameServer;
+    public int portServer;
+    public List<String> nodesToBuildChannelWith = new ArrayList<>();
+}
