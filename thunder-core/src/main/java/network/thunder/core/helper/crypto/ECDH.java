daa95cd70fa3dd756ae01eba0e8bfb9eacbe772d
matsjj <matsjj@gmail.com>
2016-05-06 16:21:45 +0100

    Use static KeyParameters for ECDH key derivation

--- thunder-core/src/main/java/network/thunder/core/helper/crypto/ECDH.java
+++ thunder-core/src/main/java/network/thunder/core/helper/crypto/ECDH.java
@@ -6,14 +6,32 @@ import org.bouncycastle.jce.provider.JCEECPrivateKey;
 import org.bouncycastle.jce.provider.JCEECPublicKey;
 
 import javax.crypto.KeyAgreement;
-import java.security.AlgorithmParameters;
-import java.security.KeyFactory;
-import java.security.Security;
+import java.security.*;
 import java.security.interfaces.ECPrivateKey;
 import java.security.interfaces.ECPublicKey;
 import java.security.spec.*;
 
 public class ECDH {
+
+    static AlgorithmParameters parameters = null;
+    static ECParameterSpec ecParameters = null;
+    static KeyFactory kf = null;
+    static KeyAgreement aKeyAgree = null;
+
+    static {
+        try {
+            Security.addProvider(new BouncyCastleProvider());
+            Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+            parameters = AlgorithmParameters.getInstance("EC", "SunEC");
+            parameters.init(new ECGenParameterSpec("secp256k1"));
+            ecParameters = parameters.getParameterSpec(ECParameterSpec.class);
+            kf = KeyFactory.getInstance("EC");
+            aKeyAgree = KeyAgreement.getInstance("ECDH");
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     /*
      * Quite some mess here to have all objects with the correct types...
@@ -21,26 +39,15 @@ public class ECDH {
     public static ECDHKeySet getSharedSecret (ECKey keyServer, ECKey keyClient) {
         try {
 
-            Security.addProvider(new BouncyCastleProvider());
-            Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
-
-            AlgorithmParameters parameters = AlgorithmParameters.getInstance("EC", "SunEC");
-            parameters.init(new ECGenParameterSpec("secp256k1"));
-            ECParameterSpec ecParameters = parameters.getParameterSpec(ECParameterSpec.class);
             ECPrivateKeySpec specPrivate = new ECPrivateKeySpec(keyServer.getPrivKey(), ecParameters);
             ECPublicKeySpec specPublic = new ECPublicKeySpec(new ECPoint(keyClient.getPubKeyPoint().getXCoord().toBigInteger(), keyClient.getPubKeyPoint()
                     .getYCoord().toBigInteger()), ecParameters);
 
-            KeyFactory kf = KeyFactory.getInstance("EC");
             ECPrivateKey privateKey = (ECPrivateKey) kf.generatePrivate(specPrivate);
             ECPublicKey publicKey = (ECPublicKey) kf.generatePublic(specPublic);
 
             JCEECPrivateKey ecPrivKey = new JCEECPrivateKey(privateKey);
             JCEECPublicKey ecPubKey = new JCEECPublicKey(publicKey);
-
-            new ECKey().getKeyCrypter();
-
-            KeyAgreement aKeyAgree = KeyAgreement.getInstance("ECDH");
 
             aKeyAgree.init(ecPrivKey);
             aKeyAgree.doPhase(ecPubKey, true);
@@ -49,10 +56,6 @@ public class ECDH {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
-//		MessageDigest hash = MessageDigest.getInstance("SHA1", "BC");
-//
-//		return hash.digest();
     }
 
 }
