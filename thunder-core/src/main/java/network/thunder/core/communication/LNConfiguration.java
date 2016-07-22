6c305ff31a1da24aecda7fe7041aeec371fdb185
matsjj <matsjj@gmail.com>
2016-07-22 09:30:28 +0100

    WIP: Move away from fixed timestamps and use block heights instead

--- thunder-core/src/main/java/network/thunder/core/communication/LNConfiguration.java
+++ thunder-core/src/main/java/network/thunder/core/communication/LNConfiguration.java
@@ -4,18 +4,19 @@ import network.thunder.core.etc.Tools;
 
 public class LNConfiguration {
     //Class for holding all the different settings with sane defaults set
+    public static int BLOCKS_PER_HOUR = 6;
 
     public int MIN_FEE_PER_BYTE = 1;
     public int DEFAULT_FEE_PER_BYTE = 5;
     public int MAX_FEE_PER_BYTE = 30;
 
-    public int MIN_REVOCATION_DELAY = 7 * 24 * 60 * 60;
-    public int DEFAULT_REVOCATION_DELAY = 2 * 7 * 24 * 60 * 60;
-    public int MAX_REVOCATION_DELAY = 4 * 7 * 24 * 60 * 60;
+    public int MIN_REVOCATION_DELAY = 7 * 24 * BLOCKS_PER_HOUR;
+    public int DEFAULT_REVOCATION_DELAY = 2 * 7 * 24 * BLOCKS_PER_HOUR;
+    public int MAX_REVOCATION_DELAY = 4 * 7 * 24 * BLOCKS_PER_HOUR;
 
-    public int MIN_REFUND_DELAY = 1 * 60 * 60;
-    public int DEFAULT_REFUND_DELAY = 3 * 60 * 60;
-    public int MAX_REFUND_DELAY = 12 * 60 * 60;
+    public int MIN_REFUND_DELAY = 1 * BLOCKS_PER_HOUR;
+    public int DEFAULT_REFUND_DELAY = 3 * BLOCKS_PER_HOUR;
+    public int MAX_REFUND_DELAY = 12 * BLOCKS_PER_HOUR;
 
     public int MIN_OVERLAY_REFUND = 2;
     public int DEFAULT_OVERLAY_REFUND = 3;
