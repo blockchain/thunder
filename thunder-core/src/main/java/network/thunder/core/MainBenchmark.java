16dc10f70c4d3904150a26177cd806263be93b94
matsjj <matsjj@gmail.com>
2016-07-22 10:19:03 +0100

    Added Benchmark program
    Allows user to select a node and have two virtual nodes connect and
    build channel with it. It will then try and make a predefined amount of
    payments as fast as possible and print the results once finished.

---
+++ thunder-core/src/main/java/network/thunder/core/MainBenchmark.java
@@ -0,0 +1,204 @@
+package network.thunder.core;
+
+import network.thunder.core.communication.NodeKey;
+import network.thunder.core.communication.ServerObject;
+import network.thunder.core.communication.layer.high.Channel;
+import network.thunder.core.communication.layer.middle.broadcasting.types.PubkeyIPObject;
+import network.thunder.core.database.DBHandler;
+import network.thunder.core.database.InMemoryDBHandler;
+import network.thunder.core.etc.Constants;
+import network.thunder.core.etc.Tools;
+import network.thunder.core.helper.PaymentRequest;
+import network.thunder.core.helper.callback.ResultCommandExt;
+import network.thunder.core.helper.callback.results.NullResultCommand;
+import network.thunder.core.helper.wallet.MockWallet;
+import org.bitcoinj.core.Coin;
+import org.bitcoinj.core.ECKey;
+import org.bitcoinj.core.Transaction;
+import org.bitcoinj.kits.WalletAppKit;
+import org.bitcoinj.script.Script;
+import org.bitcoinj.wallet.Wallet;
+import org.bitcoinj.wallet.listeners.WalletEventListener;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class MainBenchmark {
+
+    public static void main (String[] args) throws Exception {
+
+        //Let's create a ServerObject that holds the general configuration, it will get passed into each Layer of each Connection later.
+        ServerObject server1 = new ServerObject();
+        ServerObject server2 = new ServerObject();
+
+        //Currently we are only using an in-memory implementation of the DBHandler and a Wallet that is not holding real bitcoin
+        DBHandler dbHandler1 = new InMemoryDBHandler();
+        DBHandler dbHandler2 = new InMemoryDBHandler();
+
+        //Setup bitcoin testnet wallet
+        Wallet wallet1 = setupWallet();
+        Wallet wallet2 = setupWallet();
+
+        ThunderContext context1 = new ThunderContext(wallet1, dbHandler1, server1);
+        ThunderContext context2 = new ThunderContext(wallet2, dbHandler2, server2);
+
+        //Fetch IPs from other participants in the network
+        fetchNetworkIPs(context1);
+        fetchNetworkIPs(context2);
+        Thread.sleep(1000);
+
+        //New configuration, ask the user which nodes he wants to connect to..
+        String node = showIntroductionAndGetNodeList(dbHandler1);
+
+        System.out.println("MainNode.buildPaymentChannels " + node);
+        byte[] nodeKey = Tools.hexStringToByteArray(node);
+        context1.openChannel(nodeKey, new NullResultCommand());
+        Thread.sleep(100);
+        context2.openChannel(nodeKey, new NullResultCommand());
+        Thread.sleep(2000);
+
+        int TOTAL_PAYMENTS = 2000;
+
+        long startTime = System.currentTimeMillis();
+
+        for (int i = 0; i < TOTAL_PAYMENTS; i++) {
+            PaymentRequest paymentRequest2 = context2.receivePayment(1);
+            context1.makePayment(server2.pubKeyServer.getPubKey(), 1, paymentRequest2.paymentSecret, new NullResultCommand());
+        }
+
+        while (true) {
+            Channel channel = dbHandler1.getChannel(new NodeKey(nodeKey)).get(0);
+            if (channel.amountClient == (10000000 + TOTAL_PAYMENTS)) {
+
+                long endTime = System.currentTimeMillis();
+
+                double diff = (endTime - startTime);
+
+                double tps = TOTAL_PAYMENTS / (diff / 1000);
+                System.out.println("diff = " + diff);
+                System.out.println("tps = " + tps);
+                System.exit(0);
+            }
+            Thread.sleep(100);
+        }
+    }
+
+    //TODO refactor with MainNode
+    static Wallet setupWallet () {
+        if (Constants.USE_MOCK_BLOCKCHAIN) {
+            return new MockWallet(Constants.getNetwork());
+        } else {
+            //TODO somehow allow sending money out of the node again..
+            System.out.println("Setting up wallet and downloading blockheaders. This can take up to two minutes on first startup");
+            WalletAppKit walletAppKit = new WalletAppKit(Constants.getNetwork(), new File("wallet"), new String("node_"));
+            walletAppKit.startAsync().awaitRunning();
+            Wallet wallet = walletAppKit.wallet();
+            wallet.allowSpendingUnconfirmedTransactions();
+            wallet.reset();
+            System.out.println("wallet = " + wallet);
+            System.out.println("wallet.getKeyChainSeed() = " + wallet.getKeyChainSeed());
+            wallet.addEventListener(new WalletEventListener() {
+                @Override
+                public void onCoinsReceived (Wallet wallet, Transaction tx, Coin prevBalance, Coin newBalance) {
+                    System.out.println("wallet = " + wallet);
+                    System.out.println("tx = " + tx);
+                }
+
+                @Override
+                public void onCoinsSent (Wallet wallet, Transaction tx, Coin prevBalance, Coin newBalance) {
+                    System.out.println("wallet = " + wallet);
+                    System.out.println("tx = " + tx);
+                }
+
+                @Override
+                public void onReorganize (Wallet wallet) {
+
+                }
+
+                @Override
+                public void onTransactionConfidenceChanged (Wallet wallet, Transaction tx) {
+
+                }
+
+                @Override
+                public void onWalletChanged (Wallet wallet) {
+                }
+
+                @Override
+                public void onScriptsChanged (Wallet wallet, List<Script> scripts, boolean isAddingScripts) {
+
+                }
+
+                @Override
+                public void onKeysAdded (List<ECKey> keys) {
+
+                }
+            });
+            Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+                @Override
+                public void run () {
+                    try {
+                        walletAppKit.stopAsync().awaitTerminated();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }));
+            return wallet;
+        }
+    }
+
+    static void fetchNetworkIPs (ThunderContext context) {
+        boolean successful = false;
+        while (!successful) {
+            ResultCommandExt fetchNetworkListener = new ResultCommandExt();
+            context.fetchNetworkIPs(fetchNetworkListener);
+            successful = fetchNetworkListener.await().wasSuccessful();
+        }
+    }
+
+    static String showIntroductionAndGetNodeList (DBHandler dbHandler) {
+        System.out.println("Thunder.Network Benchmarking");
+        System.out.println();
+        System.out.println("Nodes currently online: ");
+
+        List<PubkeyIPObject> ipObjects = dbHandler.getIPObjects();
+
+        for (PubkeyIPObject ipObject : ipObjects) {
+            System.out.format("%10s %-50s %48s%n", "", ipObject.hostname + ":" + ipObject.port, Tools.bytesToHex(ipObject.pubkey));
+        }
+
+        if (ipObjects.size() == 1) {
+            System.out.println(ipObjects.get(0));
+            return Tools.bytesToHex(ipObjects.get(0).pubkey);
+        }
+
+        System.out.println();
+        System.out.println("Choose pubkeys of node to connect to:");
+
+        List<String> nodeList = new ArrayList<>();
+        while (true) {
+            System.out.print(">>>> ");
+            Scanner sc = new Scanner(System.in);
+            String s = sc.nextLine();
+
+            if (nodeList.contains(s)) {
+                System.out.println("Pubkey already added to the list..");
+            } else {
+                ECKey key;
+                try {
+                    key = ECKey.fromPublicOnly(Tools.hexStringToByteArray(s));
+                } catch (Exception e) {
+                    System.out.println("Invalid pubkey..");
+                    continue;
+                }
+
+                System.out.println(key.getPublicKeyAsHex() + " added to list..");
+                return s;
+            }
+
+        }
+    }
+}
