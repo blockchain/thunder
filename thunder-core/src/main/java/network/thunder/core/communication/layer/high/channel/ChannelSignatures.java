e61f101c05ffc4f0cb11e08e0bc35eaf2400a753
matsjj <matsjj@gmail.com>
2016-08-02 11:01:24 +0100

    Add serialising methods into ChannelSignatures
    We can save it as a blob into the database, as we never need to query
    for any of the fields specifically

--- thunder-core/src/main/java/network/thunder/core/communication/layer/high/channel/ChannelSignatures.java
+++ thunder-core/src/main/java/network/thunder/core/communication/layer/high/channel/ChannelSignatures.java
@@ -1,11 +1,13 @@
 package network.thunder.core.communication.layer.high.channel;
 
+import com.google.gson.Gson;
 import org.bitcoinj.crypto.TransactionSignature;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ChannelSignatures {
+    private final static Gson GSON = new Gson();
     public List<TransactionSignature> channelSignatures = new ArrayList<>();
     public List<TransactionSignature> paymentSignatures = new ArrayList<>();
 
@@ -22,5 +24,13 @@ public class ChannelSignatures {
         c.channelSignatures = new ArrayList<>(this.channelSignatures);
         c.paymentSignatures = new ArrayList<>(this.paymentSignatures);
         return c;
+    }
+
+    public String serialize () {
+        return GSON.toJson(this);
+    }
+
+    public static ChannelSignatures deserialise (String json) {
+        return GSON.fromJson(json, ChannelSignatures.class);
     }
 }
